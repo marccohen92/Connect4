95a6f87540bd89a77d6855b5c57c933c6e2a1c56
Raphaaal <azorin.raphael@gmail.com>
2020-06-30 13:29:21 +0200

    Correct transposition table read and update

--- src/intelligences/uct.py
+++ src/intelligences/uct.py
@@ -5,29 +5,59 @@ from src.common.constants import MAX_LEGAL_MOVES
 def uct_search(self, n=1000):
     #self.transposition_table = [{}, {}]
     # Commentée pour conserver la même transposition_table au cours d'un fight
+    
+    # For debug:
+    #elt_prev = self.transposition_table[self.turn - 1].get(self.hash)
+    #print("Stats for state ", self.hash, ": ", elt_prev)
+    
     for i in range(n) :
+        #print()
+        #print("========== ", i)
+        #print("========== ", i)
+        #print("========== ", i)
+        #print()
         copyBoard = copy.deepcopy(self)
         self._uct(copyBoard)
+        
     elt_state = self.transposition_table[self.turn - 1][self.hash]
+    #print("Stats for state ", self.hash, ": ", elt_state)
+    
     moves = self.legal_moves()
     best_move = moves[0]
+    best_index = 0
     best_value = elt_state["trys_per_move"][0]
     for i in range(1, len(moves)):
         if elt_state["trys_per_move"][i] > best_value:
             best_value = elt_state["trys_per_move"][i]
             best_move = moves[i]
+            best_index = i
+    #print("best_index: ", best_index)
     return best_move
 
 
 def _uct(self, board):
+    #print("=== _UCT CALL")
+    #print("State: ", board.hash)
     color = board.turn
+    #print("Color to play: ", color)
     moves = board.legal_moves()
+    #print("Nb legal moves : ", len(moves))
+    #print("Legal moves :")
+    #for move in moves:
+        #print("Color: ", move.color)
+        #print("Column: ", move.column)
+    
     if board.finished:
+        #print(" -> Terminal state")
         return board.winner
 
     running_hash = board.hash
-    if running_hash in self.transposition_table[color-1]:
-        elt_state = self.transposition_table[color-1][running_hash]
+    #print("Board current hash: ", running_hash)
+    
+    if running_hash in self.transposition_table[self.turn-1]:
+        #print(" -> Known state")
+        elt_state = self.transposition_table[self.turn-1][running_hash]
+        #print("State stats: ", elt_state)
         best_value = float("inf")
         best_move = 0
         total_playouts = elt_state["total_playouts"]
@@ -44,21 +74,35 @@ def _uct(self, board):
             if value > best_value:
                 best_value = value
                 best_move = m
+        #print("Best move index to play: ", best_move)
         res = 0.0
         if (len(moves) > 0):
-            board.play(moves[best_move])
-            res = self._uct(board)
+            
             trys = [0.0 if i != best_move else 1 for i in range(MAX_LEGAL_MOVES)]
+
+            b_simulation = copy.deepcopy(board)
+            b_simulation.play(moves[best_move])
+            
+            #print("=== Start recursive call...")
+            res = self._uct(b_simulation)
+            #print("=== ... End recursive call, result was ", res)
+            
             won = 1 if res == color else 0
             wins = [0.0 if i != best_move else won for i in range(MAX_LEGAL_MOVES)]
+            
+            
             self._update_transposition_table(running_hash, 1, trys, wins)
+            #print("Updated transposition table for hash ", running_hash)
+            #print(self.transposition_table[self.turn-1][running_hash])
 
         return res
     else:
+        #print(" -> Unknown state")
         trys = [0.0 for i in range(MAX_LEGAL_MOVES)]
         wins = [0.0 for i in range(MAX_LEGAL_MOVES)]
         self._update_transposition_table(running_hash, 1, trys, wins) 
-
+        #print("Updated transposition table for hash ", running_hash)
+        #print(self.transposition_table[self.turn-1][running_hash])
         res = board.playout()
 
         return res
