1a3e09b86b18c089f72a258a657551428377f74a
Raphaaal <azorin.raphael@gmail.com>
2020-06-30 13:29:55 +0200

    Add verbose option for contests

--- src/game/contest.py
+++ src/game/contest.py
@@ -2,7 +2,7 @@ from src.game.fight import fight
 from src.common.constants import RED, YELLOW
 from tqdm import tqdm
 
-def contest(p1, p2, nb_fights, nb_playouts):
+def contest(p1, p2, nb_fights, nb_playouts, verbose=False):
     p1_name = p1
     p2_name = p2
     if p1 == p2:
@@ -17,7 +17,7 @@ def contest(p1, p2, nb_fights, nb_playouts):
         else:
             players[RED] = p2_name
             players[YELLOW] = p1_name
-        winner = fight(engines[players[RED]], engines[players[YELLOW]], nb_playouts=nb_playouts, verbose=False)
+        winner = fight(engines[players[RED]], engines[players[YELLOW]], nb_playouts=nb_playouts, verbose=verbose)
         scores[players[winner]] +=1
     for player in scores:
         scores[player] /= nb_fights
